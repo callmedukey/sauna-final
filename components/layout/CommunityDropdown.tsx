9b850ae30dd7ed9a4d19593bee86f9cfbb149e97
callmedukey <87065409+callmedukey@users.noreply.github.com>
2024-12-05 07:32:38 +0900

    abc

--- components/layout/CommunityDropdown.tsx
+++ components/layout/CommunityDropdown.tsx
@@ -12,7 +12,7 @@ const CommunityDropdown = () => {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <button 
-          className="hover:motion-preset-shake"
+          className="hover:motion-preset-shake w-[4rem] text-center"
           aria-haspopup="true"
           aria-expanded="false"
           aria-label="커뮤니티 메뉴 열기"
@@ -20,7 +20,12 @@ const CommunityDropdown = () => {
           커뮤니티
         </button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="min-w-24 bg-siteBgGray py-1 text-center">
+      <DropdownMenuContent 
+        className="min-w-24 bg-siteBgGray py-1 text-center"
+        sideOffset={8}
+        align="center"
+        style={{ transformOrigin: "top center" }}
+      >
         <DropdownMenuItem asChild>
           <Link href="/community/notice" className="w-full justify-center">
             공지 사항
